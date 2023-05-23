cd917cb959c9abc44bd16348d498620b9fcf387d
Henrique Lobo <98746888+HLN-lobo@users.noreply.github.com>
2023-05-23 11:27:12 -0300

    Add files via upload

--- Simulado/script.js
+++ Simulado/script.js
@@ -1,14 +1,104 @@
-var btn = document.getElementById('action-btn');
-var container = document.querySelector('container');
+var btn = document.getElementById('orfa-btn');
+var container = document.querySelector('container-orfa');
 
 btn.addEventListener("click", function(){
 
-    var container = document.getElementById("container");
+    var container = document.getElementById("container-orfa");
 
-    if(container.style.display ==="none"){
-        container.style.display = "inline-block";
-    } else{
+    if(container.style.display ==="flex"){
         container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('querida-btn');
+var container = document.querySelector('container-querida');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-querida");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('mulher-btn');
+var container = document.querySelector('container-mulher');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-mulher");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('telefone-btn');
+var container = document.querySelector('container-telefone');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-telefone");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('avatar-btn');
+var container = document.querySelector('container-avatar');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-avatar");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('sorria-btn');
+var container = document.querySelector('container-sorria');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-sorria");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
+    }
+
+});
+
+var btn = document.getElementById('queda-btn');
+var container = document.querySelector('container-queda');
+
+btn.addEventListener("click", function(){
+
+    var container = document.getElementById("container-queda");
+
+    if(container.style.display ==="flex"){
+        container.style.display = "none";
+    } else{
+        container.style.display = "flex";
     }
 
 });
--- Simulado/simulado.php
+++ Simulado/simulado.php
@@ -25,6 +25,7 @@
             display: flex;
             justify-content: center;
             align-items: start;
+            flex-wrap:  wrap
         }
         .movie-card {
             width: 200px;
@@ -33,18 +34,36 @@
             margin: 10px;
             padding: 10px;
             float: left;
-            
+            border: solid 2px;
+            border-radius: 8px;
         }
         .movie-card img {
             width: 100%;
             height: 290px;
         }
-        .movie-card h3 {
+        .movie-card .btn-compra, button h3 {
             font-size: 15px;
             margin: 0;
             padding: 10px 0;
             text-align: center;
             color: #000;
+            cursor: pointer;
+        }
+        .movie-card .btn-compra:hover {
+            transition: linear 0.2s;
+            background-color: #ca221f;
+        }
+        .movie-card button:hover {
+            background-color: #d8d8d8;
+        }
+        .movie-card div h3 {
+            font-size: 15px;
+            margin: 0;
+            padding: 10px 0;
+            color:#941f1f;
+        }
+        .movie-card div {
+            transition: ease 2s;
         }
         .movie-card a{
             text-decoration: none
@@ -59,6 +78,22 @@
             width: auto;
             height: auto;
         }
+        .movie-card div{
+            display: none;
+        }
+        button{
+            display: flex;
+            width: 100%;
+            align-items: center;
+            justify-content: center;
+            margin-top: 10px;
+            border: solid 2px;
+            border-radius: 8px;
+        }
+        .movie-card .btn-compra{
+            /* border: solid 2px; */
+            border-radius: 8px;
+        }
 
     </style>
 </head>
@@ -71,34 +106,44 @@
         <div class="main">
             <div class="movie-card">
                 <img src="./img/orfa.jpg" alt="Filme 1">
-                <h3>A Orfã 2</h3>
                 <a href=""><h3 class="btn-compra">Comprar</h3></a>
-                <button id="action-btn"><h3>Sinopse</h3></button>
-                <div id="container">Esther consegue escapar de um centro psiquiátrico onde está confinada e viaja para os Estados Unidos se passando por filha desaparecida de uma família rica. No entanto, uma mãe superprotetora fará de tudo por sua família, não importando os meios.</div>
+                <button id="orfa-btn"><h3>Sinopse</h3></button>
+                <div id="container-orfa"><h3>Esther consegue escapar de um centro psiquiátrico onde está confinada e viaja para os Estados Unidos se passando por filha desaparecida de uma família rica. No entanto, uma mãe superprotetora fará de tudo por sua família, não importando os meios.</h3></div>
             </div>
             <div class="movie-card">
                 <img src="./img/naosepreocupe.jpg" alt="Filme 2">
-                <h3>Não Se Preocupe, Querida</h3>
+                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="querida-btn"><h3>Sinopse</h3></button>
+                <div id="container-querida"><h3>Uma dona de casa que vive em uma comunidade experimental começa a suspeitar que a empresa de seu marido está escondendo segredos perturbadores.</h3></div>
             </div>
             <div class="movie-card">
-                <img src="./img/AmulherRei.jpg" alt="Filme 3">
-                <h3>A Mulher Rei</h3>
+                <img src="./img/AmulherRei.jpg" alt="Filme 3"></h3>
+                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="mulher-btn"><h3>Sinopse</h3></button>
+                <div id="container-mulher"><h3>Em 1800, o general Nanisca treina um grupo de mulheres guerreiras para proteger o reino africano de Dahomey de um inimigo estrangeiro.</h3></div>
             </div>
             <div class="movie-card">
                 <img src="./img/otelefone.jpg" alt="Filme 4">
-                <h3>O Telefone Preto</h3>
+                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="telefone-btn"><h3>Sinopse</h3></button>
+                <div id="container-telefone"><h3>Finney Shaw, de 13 anos, é sequestrado por um sádico assassino mascarado e mantido em um porão à prova de som. Até que um telefone desconectado na parede começa a tocar, e ele logo descobre que pode ouvir as vozes das vítimas anteriores do maníaco.</h3></div>
             </div>
             <div class="movie-card">
                 <img src="./img/Avatar2.jpg" alt="Filme 5">
-                <h3>Avatar 2</h3>
+                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="avatar-btn"><h3>Sinopse</h3></button>
+                <div id="container-avatar"><h3>Após formar uma família, Jake Sully e Ney'tiri fazem de tudo para ficarem juntos. No entanto, eles devem sair de casa e explorar as regiões de Pandora quando uma antiga ameaça ressurge, e Jake deve travar uma guerra difícil contra os humanos.</h3></div>
             </div>
             <div class="movie-card">
-                <img src="./img/Sorria.jpg" alt="Filme 6">
-                <h3>Sorria</h3>
+                <img src="./img/Sorria.jpg" alt="Filme 6">                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="sorria-btn"><h3>Sinopse</h3></button>
+                <div id="container-sorria"><h3>Após um paciente cometer um suicídio brutal em sua frente, a psiquiatra Rose é perseguida por uma entidade maligna que muda de forma. Enquanto tenta escapar desse pesadelo, Rose também precisa enfrentar seu passado conturbado para sobreviver.</h3></div>
             </div>
             <div class="movie-card">
                 <img src="./img/Queda.jpg" alt="Filme 7">
-                <h3>A Queda</h3>
+                <a href=""><h3 class="btn-compra">Comprar</h3></a>
+                <button id="queda-btn"><h3>Sinopse</h3></button>
+                <div id="container-queda"><h3>Para as melhores amigas Becky e Hunter, a vida é sobre vencer medos e ultrapassar limites. No entanto, depois de subir 600 metros até o topo de uma torre de rádio remota e abandonada, elas acabam presas lá em cima. Agora, suas habilidades de escalada são colocadas à prova enquanto lutam desesperadamente para sobreviver aos elementos, a falta de suprimentos e a altura vertiginosa.</h3></div>
             </div>
         </div>
     </main>
